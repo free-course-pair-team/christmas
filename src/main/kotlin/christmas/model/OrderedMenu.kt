59f6d6dc54dfb6cc16e9aa797c379b6f409a1e84
DaeYoung Jeong <eodud1025@naver.com>
2024-12-05 15:55:16 +0900

    feat(OrderedMenu): 금액 반환 기능

--- src/main/kotlin/christmas/model/OrderedMenu.kt
+++ src/main/kotlin/christmas/model/OrderedMenu.kt
@@ -1,3 +1,38 @@
 package christmas.model
 
-data class OrderMenu(val name: String, val quantity: Int)
+import christmas.model.constant.Category
+
+class OrderedMenu(val name: String, val quantity: Int) {
+
+    fun getAmount(): Int {
+        val menu =
+            requireNotNull(Category.entries.map { it.menu }.flatten().find { it.first == name })
+        return menu.second * quantity
+    }
+
+    override fun toString() =
+        "$name ${quantity}개"
+
+    companion object {
+        fun from(inputMenu: List<String>):OrderedMenu {
+            require(inputMenu.size == 2) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+
+            val name = validateMenu(inputMenu[0])
+            val quantity = validateQuantity(inputMenu[1])
+
+            return OrderedMenu(name, quantity)
+        }
+
+        private fun validateQuantity(q: String): Int {
+            val intQ = requireNotNull(q.toIntOrNull()) {"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
+            require(intQ >= 1) {"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
+            return intQ
+        }
+
+        private fun validateMenu(menu: String): String {
+            val menuList = Category.entries.map { menuList -> menuList.menu.map { it.first } }.flatten()
+            require(menuList.contains(menu)) {"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
+            return menu
+        }
+    }
+}
