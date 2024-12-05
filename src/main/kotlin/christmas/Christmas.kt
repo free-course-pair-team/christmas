33fa1cbd386479e56b10bd36b7bcd9de8f6c3e57
김의현 <hyunkim6790@gmail.com>
2024-12-05 14:53:43 +0900

    feat(Christmas): 날짜 입력 검증 함수 구현

---
+++ src/main/kotlin/christmas/Christmas.kt
@@ -0,0 +1,30 @@
+package christmas
+
+import christmas.Month.DECEMBER
+
+class Christmas(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun start() {
+        outputView.printWelcomeMessage()
+        val visitDay = validateVisitDay()
+        val (menu, menuCount) = inputView.readMenuAndCount()
+    }
+
+    fun validateVisitDay(): Int = retryInput {
+        val rawVisitDay = inputView.readVisitDay()
+        val visitDay = requireNotNull(rawVisitDay.toIntOrNull()) {"[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."}
+        require(visitDay in DECEMBER.startDay..DECEMBER.endDay) {"[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."}
+        visitDay
+    }
+
+//    fun validateMenuAndCount(): Pair<String, Int> = retryInput {
+//        val rawMenuAndCount = inputView.readMenuAndCount().split(",")
+//        val menuAndCount = rawMenuAndCount.map { OrderMenu(it.split("-")[0], it.split("-")[1].toInt())}
+//        require(menuAndCount.size == 2) {"[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
+//        val menu = menuAndCount.first()
+//        menu to
+//    }
+
+}
