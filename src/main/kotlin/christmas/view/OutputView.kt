8e0d92f0118e0e0c0fa2f0f9368643113d9c0230
DaeYoung Jeong <eodud1025@naver.com>
2024-12-05 15:56:35 +0900

    feat(OutputView): 주문한 음식, 할인전 주문 금액 출력

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,19 +1,33 @@
-package christmas
+package christmas.view
 
-import java.awt.SystemColor.menu
+import christmas.model.OrderedMenu
+import christmas.model.constant.Month
+import christmas.util.toWonFormat
 
 class OutputView {
-    fun printWelcomeMessage() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`")
+    fun printWelcomeMessage() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
 
     fun printDayToEvent(day: Int) {
-        println("12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
+        println("${Month.DECEMBER.value}월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
     }
 
-    fun printOrderMenu(orderMenu: List<OrderMenu>) {
-        orderMenu.forEach { (menu, quantity) ->
-            println("$menu $quantity")
+    fun printOrderMenu(orderedMenus: List<OrderedMenu>) {
+        println("<주문 메뉴>")
+        orderedMenus.forEach { println(it.toString()) }
+        println()
+    }
+
+    fun printBeforeAmount(amount: Int) {
+        println("<할인 전 총주문 금액>")
+        println("${amount.toWonFormat()}원")
+    }
+
+    fun printPresentChampagne(amount: Int) {
+        println("<증정 메뉴>")
+        if (amount >= 120_000) {
+            println("샴페인 1개\n")
+            return
         }
+        println("없음\n")
     }
-
-    fun printBeforeAmount() {}
 }
