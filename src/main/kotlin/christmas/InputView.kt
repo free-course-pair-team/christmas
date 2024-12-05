96b89a4c5bccbe62312a7c3c05c881b32100bc09
김의현 <hyunkim6790@gmail.com>
2024-12-05 14:10:25 +0900

    feat(InputView): 메뉴 입력 기능 구현

--- src/main/kotlin/christmas/InputView.kt
+++ src/main/kotlin/christmas/InputView.kt
@@ -9,4 +9,8 @@ class InputView {
         return Console.readLine().trim()
     }
 
+    fun readMenuAndCount(): String {
+        println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
+        return Console.readLine()
+    }
 }
