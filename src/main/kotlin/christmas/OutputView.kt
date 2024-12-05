b2ea380090a16daaf115677e9ac160f55fb85d6d
김의현 <hyunkim6790@gmail.com>
2024-12-05 14:54:01 +0900

    .

--- src/main/kotlin/christmas/OutputView.kt
+++ src/main/kotlin/christmas/OutputView.kt
@@ -1,9 +1,19 @@
 package christmas
 
+import java.awt.SystemColor.menu
+
 class OutputView {
     fun printWelcomeMessage() = println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`")
 
     fun printDayToEvent(day: Int) {
         println("12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
     }
+
+    fun printOrderMenu(orderMenu: List<OrderMenu>) {
+        orderMenu.forEach { (menu, quantity) ->
+            println("$menu $quantity")
+        }
+    }
+
+    fun printBeforeAmount() {}
 }
