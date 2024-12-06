124c8a6acddc82988d64cb449eb549e32917f64d
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:24:22 +0900

    rafactor(BenefitManager): 10줄 넘는 함수 리팩터링 및 상수화

--- src/main/kotlin/christmas/model/BenefitManager.kt
+++ src/main/kotlin/christmas/model/BenefitManager.kt
@@ -1,52 +1,59 @@
 package christmas.model
 
 import christmas.model.constant.Category
+import christmas.model.constant.Category.Companion.getChampagnePrice
 import christmas.model.constant.DayOfWeek
 import christmas.model.constant.SpecialDay
 
 class BenefitManager {
 
     fun discountAll(visitDate: Int, orderedMenus: List<OrderedMenu>): List<Pair<String, Int>> {
-        val discountInfoList = mutableListOf<Pair<String,Int>>()
-        val discountWeekdaysOrWeekends = discountWeekdaysOrWeekends(visitDate, orderedMenus)
-        val discountSpecialDays = discountSpecialDays(visitDate)
-        val discountChristmasDay = discountChristmasDay(visitDate)
-        val presentEvent = presentEvent(orderedMenus.sumOf { it.getAmount() })
-        if (discountWeekdaysOrWeekends.first != NONE) discountInfoList.add(discountWeekdaysOrWeekends)
-        if (discountSpecialDays.first != NONE) discountInfoList.add(discountSpecialDays)
-        if (discountChristmasDay.first != NONE) discountInfoList.add(discountChristmasDay)
-        if (presentEvent.first != NONE) discountInfoList.add(presentEvent)
+        val discountInfoList = mutableListOf<Pair<String, Int>>()
+        val discountContents = listOf<Pair<String, Int>>(
+            discountWeekdaysOrWeekends(visitDate, orderedMenus),
+            discountSpecialDays(visitDate),
+            discountChristmasDay(visitDate),
+            presentEvent(orderedMenus.sumOf { it.getAmount() }),
+        )
+        discountContents.forEach { applyDiscount(it, discountInfoList) }
         return discountInfoList.toList()
+    }
+
+    private fun applyDiscount(discount: Pair<String, Int>, discountInfoList: MutableList<Pair<String, Int>>) {
+        if (discount.second != NOT_DISCOUNT_AMOUNT) discountInfoList.add(discount)
     }
 
     // 평일, 주말 확인 후 할인
     fun discountWeekdaysOrWeekends(visitDate: Int, orderedMenus: List<OrderedMenu>): Pair<String, Int> {
-        return when((visitDate-1)%DayOfWeek.entries.size) {
+        return when ((visitDate - 1) % DayOfWeek.entries.size) {
             in DayOfWeek.SUNDAY.ordinal..DayOfWeek.THURSDAY.ordinal -> {
-                WEEKDAYS to orderedMenus.filter { it.getCategory() == Category.DESSERT }.sumOf { it.quantity }* 2_023
+                WEEKDAYS to orderedMenus.filter { it.getCategory() == Category.DESSERT }.sumOf { it.quantity } * 2_023
             }
+
             in DayOfWeek.FRIDAY.ordinal..DayOfWeek.SATURDAY.ordinal -> {
                 WEEKENDS to orderedMenus.filter { it.getCategory() == Category.MAIN }.sumOf { it.quantity } * 2_023
             }
-            else -> NONE to 0
+
+            else -> NONE to NOT_DISCOUNT_AMOUNT
         }
     }
 
     fun discountSpecialDays(visitDate: Int): Pair<String, Int> {
-        if (SpecialDay.entries.any {it.value == visitDate}) return SPECIAL to SPECIAL_DISCOUNT_AMOUNT
-        return NONE to 0
+        if (SpecialDay.entries.any { it.value == visitDate }) return SPECIAL to SPECIAL_DISCOUNT_AMOUNT
+        return NONE to NOT_DISCOUNT_AMOUNT
     }
 
     fun discountChristmasDay(visitDate: Int): Pair<String, Int> {
-        if (visitDate >= 26) return NONE to 0
-        return CHRISTMAS_DAY to 1_000 + (visitDate-1) * 100
+        if (visitDate >= EVENT_END_DAY) return NONE to NOT_DISCOUNT_AMOUNT
+        return CHRISTMAS_DAY to 1_000 + (visitDate - 1) * 100
     }
 
-    fun presentEvent(amount: Int): Pair<String, Int> {
+    private fun presentEvent(amount: Int): Pair<String, Int> {
         if (amount >= 120_000) {
-            return PRESENT to 25000
+            return PRESENT to getChampagnePrice()
         }
-        return NONE to 0
+
+        return NONE to NOT_DISCOUNT_AMOUNT
     }
 
     companion object {
@@ -57,5 +64,7 @@ class BenefitManager {
         const val PRESENT = "증정 이벤트"
         const val NONE = ""
         const val SPECIAL_DISCOUNT_AMOUNT = 1_000
+        const val NOT_DISCOUNT_AMOUNT = 0
+        const val EVENT_END_DAY = 26
     }
 }
