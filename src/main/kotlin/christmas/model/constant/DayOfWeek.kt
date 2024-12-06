ad31e7930ad9375a4805626cc94d2429e8c79866
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:38:33 +0900

    refactor(DayOfWeek): sortedDayOfWeek 함수 삭제

--- src/main/kotlin/christmas/model/constant/DayOfWeek.kt
+++ src/main/kotlin/christmas/model/constant/DayOfWeek.kt
@@ -8,12 +8,4 @@ enum class DayOfWeek() {
     TUESDAY,
     WEDNESDAY,
     THURSDAY;
-
-    companion object {
-        fun sortedDayOfWeek(): List<DayOfWeek> {
-            val days = DayOfWeek.entries.drop(5)+DayOfWeek.entries.take(5)
-            println("days: $days")
-            return days
-        }
-    }
 }
