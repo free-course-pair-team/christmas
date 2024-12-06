90c0ead8daf8506420711b294f1c70f33b3a6f2d
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:21:43 +0900

    refactor(Christmas): 에러처리 추가 및 리팩터링

--- src/main/kotlin/christmas/controller/Christmas.kt
+++ src/main/kotlin/christmas/controller/Christmas.kt
@@ -2,6 +2,8 @@ package christmas.controller
 
 import christmas.model.BenefitManager
 import christmas.model.OrderedMenu
+import christmas.model.constant.Badge
+import christmas.model.constant.Category
 import christmas.model.constant.Month.DECEMBER
 import christmas.util.retryInput
 import christmas.view.InputView
@@ -17,16 +19,30 @@ class Christmas(
         val visitDay = validateVisitDay() // 방문 일
         val orderedMenus = validateMenuAndCount() // 주문 메뉴
         outputView.printDayToEvent(visitDay)
+        printReceipt(orderedMenus, visitDay)
+    }
+
+    private fun printReceipt(orderedMenus: List<OrderedMenu>, visitDay: Int) {
         outputView.printOrderMenu(orderedMenus)
         val orderedMenusBeforeAmount = orderedMenus.sumOf { it.getAmount() } // 총 금액
         outputView.printBeforeAmount(orderedMenusBeforeAmount)
         outputView.printPresentChampagne(orderedMenusBeforeAmount)
-        // 방문일과 메뉴, 총금액도?
-        val benefitInfo = canCheckDiscountAll(visitDay, orderedMenus) // 할인 혜택 내역
+        val benefitInfo = canCheckDiscountAll(visitDay, orderedMenus, orderedMenusBeforeAmount) // 할인 혜택 내역
         outputView.printBenefitsInfo(benefitInfo)
         outputView.printAllBenefitsAmount(benefitInfo)
         val totalAmountAfterDiscount = calculateTotalAmountAfterDiscount(orderedMenusBeforeAmount, benefitInfo)
-        outputView.expectPaymentAfterDiscount(totalAmountAfterDiscount)
+        outputView.printExpectPaymentAfterDiscount(totalAmountAfterDiscount)
+        outputView.printEventBadge(eventBadge(benefitInfo))
+    }
+
+    private fun eventBadge(benefitInfo: List<Pair<String, Int>>): String {
+        val totalBenefits = benefitInfo.sumOf { it.second }
+        return when {
+            totalBenefits >= Badge.SANTA.applyAmount -> Badge.SANTA.value
+            totalBenefits >= Badge.TREE.applyAmount -> Badge.TREE.value
+            totalBenefits >= Badge.STAR.applyAmount -> Badge.STAR.value
+            else -> Badge.NONE.value
+        }
     }
 
     private fun validateVisitDay(): Int = retryInput {
@@ -37,19 +53,25 @@ class Christmas(
         visitDay
     }
 
-    fun validateMenuAndCount(): List<OrderedMenu> = retryInput {
+    private fun validateMenuAndCount(): List<OrderedMenu> = retryInput {
         val rawMenuAndCount = inputView.readMenuAndCount().split(",")
-        rawMenuAndCount.map { OrderedMenu.from(it.split("-")) }
+        val orderedMenu = rawMenuAndCount.map { OrderedMenu.from(it.split("-")) }
+        require(orderedMenu.all { it.getCategory() == Category.BEVERAGE  }.not() ) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
+        require(orderedMenu.sumOf { it.quantity } < 20) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."}
+        orderedMenu
     }
 
-    fun canCheckDiscountAll(visitDate: Int, orderedMenus: List<OrderedMenu>) =
-        benefitManager.discountAll(visitDate, orderedMenus)
 
-    fun calculateTotalAmountAfterDiscount(beforeAmount: Int, benefitInfo: List<Pair<String, Int>>): Int {
+    private fun canCheckDiscountAll(visitDate: Int, orderedMenus: List<OrderedMenu>, orderedMenusBeforeAmount: Int): List<Pair<String,Int>> {
+        if (orderedMenusBeforeAmount <= 10000) {
+            return emptyList()
+        }
+        return benefitManager.discountAll(visitDate, orderedMenus)
+    }
+
+    private fun calculateTotalAmountAfterDiscount(beforeAmount: Int, benefitInfo: List<Pair<String, Int>>): Int {
         val benefitInfoRemovedChampagne = benefitInfo.filter { it.first != BenefitManager.PRESENT }
         val benefitAmount = benefitInfoRemovedChampagne.sumOf { it.second }
         return beforeAmount - benefitAmount
     }
-
-
 }
