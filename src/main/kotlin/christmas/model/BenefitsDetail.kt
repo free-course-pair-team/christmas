c21e29b779dc34ff69c0caa1ebc386620e92f1f4
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:42:04 +0900

    refactor(BenefitDetail): 사용하지 않는 데이터 클래스 삭제

--- src/main/kotlin/christmas/model/BenefitsDetail.kt
+++
@@ -1,8 +0,0 @@
-package christmas.model
-
-data class BenefitsDetail(
-    val dayDiscount: Int,
-    val menuDiscount: Int?,
-    val specialDiscount: Boolean = false,
-    val presentEvent: Boolean = false
-)
