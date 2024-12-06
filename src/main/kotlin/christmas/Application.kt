6da4f4cbfdffeb50845a656b3e388659bbe5e2e9
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:20:59 +0900

    style(Application): 주석 삭제

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -6,6 +6,5 @@ import christmas.view.InputView
 import christmas.view.OutputView
 
 fun main() {
-    // 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.
     Christmas(inputView = InputView(), outputView = OutputView(), benefitManager = BenefitManager()).start()
 }
