5f8252a771909be146b36b8148d16ec9f0ca9f4e
김의현 <hyunkim6790@gmail.com>
2024-12-06 16:35:53 +0900

    refactor(SepcialDay): 상수명 변경

--- src/main/kotlin/christmas/model/constant/SpecialDay.kt
+++ src/main/kotlin/christmas/model/constant/SpecialDay.kt
@@ -1,5 +1,10 @@
 package christmas.model.constant
 
 enum class SpecialDay(val value: Int) {
-    FIRST(3), SECOND(10), THIRD(17), FOUR(24), FIFTH(25), SIXTH(31)
+    THREE_DAYS(3),
+    TEN_DAYS(10),
+    SEVENTEEN_DAYS(17),
+    TWENTY_FOURTH_DAY(24),
+    TWENTY_FIVE_DAY(25),
+    THIRTY_FIRST(31),
 }
